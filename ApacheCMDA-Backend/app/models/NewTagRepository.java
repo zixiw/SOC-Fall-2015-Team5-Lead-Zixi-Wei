76f90c2c0b2764a8b66dcfa919662626a73f7c8c
Skywalker <lingmingyang1@hotmail.com>
2015-12-15 15:57:12 -0500

    Update NewTagRepository.java

--- ApacheCMDA-Backend/app/models/NewTagRepository.java
+++ ApacheCMDA-Backend/app/models/NewTagRepository.java
@@ -6,7 +6,7 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 
 /**
- * Created by zixiwei on 11/30/15.
+ * Created by Zixi Wei, Mingyang Ling, Zheng Lei, Sen Yue on 11/30/15.
  */
 @Named
 @Singleton
